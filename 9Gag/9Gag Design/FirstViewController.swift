4b1f908bd770610dccadb34add517505c96ff8bb
Mehmet <kurtgozmehmet159@gmail.com>
2019-02-04 20:56:46 +0300

    init

---
+++ 9Gag/9Gag Design/FirstViewController.swift
@@ -0,0 +1,34 @@
+//
+//  FirstViewController.swift
+//  9Gag Design
+//
+//  Created by Atil Samancioglu on 22/06/2017.
+//  Copyright © 2017 Atil Samancioglu. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+
+}
+
